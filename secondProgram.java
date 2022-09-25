b0915771f8dc4687ee2e70cab45c703fc161f0e9
Rashmi896 <78081648+Rashmi896@users.noreply.github.com>
2022-09-25 12:24:55 +0530

    Update secondProgram.java

--- secondProgram.java
+++ secondProgram.java
@@ -2,6 +2,7 @@
 
 public class secondProgram{
     public static void main(String[] args) {
+        //determine which of the three numbers is the smallest number, and displays that number using if-else statement.
         int firstnumber=5,secondnumber = 10 , thirdnumber=15;
         if(firstnumber<secondnumber &&firstnumber<thirdnumber)
         System.out.println( firstnumber + " is the smallest number");
