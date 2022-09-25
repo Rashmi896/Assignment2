8ae7053e5db2c2183bae446328e1145a3b4de3ae
Rashmi896 <78081648+Rashmi896@users.noreply.github.com>
2022-09-25 12:28:23 +0530

    Update sixthProgram.java

--- sixthProgram.java
+++ sixthProgram.java
@@ -1,5 +1,6 @@
 public class sixthProgram {
     public static void main(String[] args) {
+        //print out all the multiples of 3 from 3 to 36
         int i=1;
         do{
             System.out.print(i*3+" ");
