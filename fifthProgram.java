0a97fea693b797e6d6f07c7b835eaced7438cfc1
Rashmi896 <78081648+Rashmi896@users.noreply.github.com>
2022-09-25 12:16:36 +0530

    Update fifthProgram.java

--- fifthProgram.java
+++ fifthProgram.java
@@ -1,14 +1,14 @@
 public class fifthProgram{
     public static void main(String[] args) {
-        System.out.print("n n^2 n^3 n^4");
-        int n=5;
+        System.out.print("n n^2 n^3 n^4");// According to format
+        int n=5; // prints the table for 5 numbers
         for(int i=0;i<=n;i++)
         {
-           System.out.println(" ");
-            System.out.print(Math.pow(i,1)+" ");
-            System.out.print(Math.pow(i,2)+" ");
-            System.out.print(Math.pow(i,3)+" ");
-            System.out.print(Math.pow(i,4)+" ");
+           System.out.println(" "); // prints in new line
+            System.out.print(Math.pow(i,1)+" "); // prints number raised to power 1
+            System.out.print(Math.pow(i,2)+" ");// prints number raised to power 2
+            System.out.print(Math.pow(i,3)+" ");// prints number raised to power 3
+            System.out.print(Math.pow(i,4)+" ");// prints number raised to power 4
 
 
         }
