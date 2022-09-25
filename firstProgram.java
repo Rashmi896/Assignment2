d01f2c6aa020479afca7a0d8df53b9ac7e18f5b5
Rashmi896 <78081648+Rashmi896@users.noreply.github.com>
2022-09-25 12:23:28 +0530

    Update firstProgram.java

--- firstProgram.java
+++ firstProgram.java
@@ -2,6 +2,7 @@ public class firstProgram
 {
     public static void main(String[] args) 
 {
+   //increases the score by 5 marks if score is between 80 and 90
    double score = 85;
    if (score>80 && score<90)
    {
